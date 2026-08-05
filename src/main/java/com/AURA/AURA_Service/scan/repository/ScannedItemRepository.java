5b86a6e2484f287d44ed132cd6766036461781ca
Geonwoo-Standard <gyn9208@gachon.ac.kr>
2026-08-06 01:53:37 +0900

    [FEAT] GET /api/storage/items 개발

--- src/main/java/com/AURA/AURA_Service/scan/repository/ScannedItemRepository.java
+++ src/main/java/com/AURA/AURA_Service/scan/repository/ScannedItemRepository.java
@@ -2,6 +2,7 @@ package com.AURA.AURA_Service.scan.repository;
 
 import com.AURA.AURA_Service.scan.domain.ScannedItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ScannedItemRepository extends JpaRepository<ScannedItem, Long> {
+public interface ScannedItemRepository extends JpaRepository<ScannedItem, Long>, JpaSpecificationExecutor<ScannedItem> {
 }
